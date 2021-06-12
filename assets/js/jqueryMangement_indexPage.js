0d03ebce3fc5e17f90dcc2c130ac7e813dc39240
GaoLAQ <gao@printexpress.co.uk>
2021-06-12 23:52:34 +0100

    feat: :art: create image and querykey jason and made js file

--- assets/js/jqueryMangement_indexPage.js
+++ assets/js/jqueryMangement_indexPage.js
@@ -1,34 +1,116 @@
 $(document).ready(function () {
   // any js to go here
+  
+  function generateElement() {
+    const indexOffers = [
+      {
+        id: "1",
+        imageURL: "assets/images/life-logos/cheshire-life.svg",
+        queryKeyName: "cheshire",
+      },
+      {
+        id: "2",
+        imageURL: "assets/images/life-logos/cornwall-life.svg",
+        queryKeyName: "cornwall",
+      },
+      {
+        id: "3",
+        imageURL: "assets/images/life-logos/cotswold-life.svg",
+        queryKeyName: "cotswold",
+      },
+      {
+        id: "4",
+        imageURL: "assets/images/life-logos/derbyshire-life.svg",
+        queryKeyName: "derbyshire",
+      },
+      {
+        id: "5",
+        imageURL: "assets/images/life-logos/devon-life.svg",
+        queryKeyName: "devon",
+      },
+      {
+        id: "6",
+        imageURL: "assets/images/life-logos/dorset-life.svg",
+        queryKeyName: "dorset",
+      },
+      {
+        id: "7",
+        imageURL: "assets/images/life-logos/essex-life.svg",
+        queryKeyName: "essex",
+      },
+      {
+        id: "8",
+        imageURL: "assets/images/life-logos/hampshire-life.svg",
+        queryKeyName: "hampshire",
+      },
+      {
+        id: "9",
+        imageURL: "assets/images/life-logos/hertfordshire-life.svg",
+        queryKeyName: "hertfordshire",
+      },
+      {
+        id: "10",
+        imageURL: "assets/images/life-logos/kent-life.svg",
+        queryKeyName: "kent",
+      },
+      {
+        id: "11",
+        imageURL: "assets/images/life-logos/norfolk.svg",
+        queryKeyName: "norfolk",
+      },
+      {
+        id: "12",
+        imageURL: "assets/images/life-logos/somerset-life.svg",
+        queryKeyName: "somerset",
+      },
+      {
+        id: "13",
+        imageURL: "assets/images/life-logos/suffolk-magazine.svg",
+        queryKeyName: "suffolk",
+      },
+      {
+        id: "14",
+        imageURL: "assets/images/life-logos/surrey-life.svg",
+        queryKeyName: "surrey",
+      },
+      {
+        id: "15",
+        imageURL: "assets/images/life-logos/sussex-life.svg",
+        queryKeyName: "sussex",
+      },
+      {
+        id: "16",
+        imageURL: "assets/images/life-logos/yorkshire-life.svg",
+        queryKeyName: "yorkshire",
+      },
+    ];
+    indexOffers.forEach(function (child) {
+      var list = $("#main").append(
+        $("<div></div>")
+          .attr({
+            class: "col-6 col-sm-6 col-md-3",
+          })
+          .append(
+            $("<a></a>")
+              .attr({
+                class: "btn btn-block btn-primary mt-3",
+                href: "store-page.html?brand=" + child.queryKeyName.toString(),
+              })
+              .append(
+                $("<img></img>").attr({
+                  class: "logo-svg-button",
+                  alt: "Logo",
+                  src: child.imageURL.toString(),
+                })
+              )
+          )
+      );
+      return list;
+    });
+  }
+  generateElement();
 
-  // Add div Tag
-  var mainBody = $("#main");
-
-  var divTag = $("<div></div>").attr({
-    class: "col-6 col-sm-6 col-md-3",
-  });
-  mainBody.append(divTag);
-
-  // Add a Tag
-  var aTag = $("<a></a>").attr({
-    class: "btn btn-block btn-primary mt-3",
-    href: "store-page.html?brand=cheshire",
-  });
-  divTag.append(aTag);
-
-  // Add Image Tag
-  var imageTag = $("<img></img>").attr({
-    class: "logo-svg-button",
-    alt: "Logo",
-    src: "assets/images/life-logos/cheshire-life.svg",
-  });
-  aTag.append(imageTag);
-
-  // function generateHtml(){
-  //    for(var i=0;i<subsrtibutionList.length; i++){
-  //       console.log(subsrtibutionList)
-  //    }
-  // }
+  // function appendChildren(indexOffers) {}
 
   function getQueryVariable(variable) {
     var query = window.location.search.substring(1);
